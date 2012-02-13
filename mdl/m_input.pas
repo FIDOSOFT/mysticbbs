00d76ab2571b4262941b94886fd2d96702732947
mysticbbs <mysticbbs@gmail.com>
2012-02-13 18:51:21 -0500

    Initial import

---
+++ mdl/m_input.pas
@@ -0,0 +1,72 @@
+Unit m_Input;
+
+{$I M_OPS.PAS}
+
+Interface
+
+{$IFDEF WINDOWS}
+  Uses
+    m_Input_Windows;
+
+  Type
+    TInput = Class(TInputWindows);
+{$ENDIF}
+
+{$IFDEF LINUX}
+  Uses m_Input_Linux;
+
+  Type
+    TInput = Class(TInputLinux);
+{$ENDIF}
+
+{$IFDEF DARWIN}
+  Uses m_Input_Darwin;
+
+  Type
+    TInput = Class(TInputDarwin);
+{$ENDIF}
+
+Const
+  keyALTA  = #30;
+  keyALTB  = #48;
+  keyALTC  = #46;
+  keyALTD  = #32;
+  keyALTE  = #18;
+  keyALTH  = #35;
+  keyALTI  = #23;
+  keyALTJ  = #36;
+  keyALTL  = #38;
+  keyALTM  = #50;
+  keyALTO  = #24;
+  keyALTQ  = #16;
+  keyALTR  = #19;
+  keyALTS  = #31;
+  keyALTT  = #20;
+  keyALTX  = #45;
+  keyALTY  = #21;
+  keyALTZ  = #44;
+
+  keyF1    = #59;
+  keyF2    = #60;
+  keyF3    = #61;
+  keyF4    = #62;
+  keyF5    = #63;
+  keyF6    = #64;
+  keyF7    = #65;
+  keyF8    = #66;
+  keyF9    = #67;
+  keyF10   = #68;
+
+  keyUP    = #72;
+  keyDOWN  = #80;
+  keyLEFT  = #75;
+  keyRIGHT = #77;
+  keyPGUP  = #73;
+  keyPGDN  = #81;
+  keyHOME  = #71;
+  keyEND   = #79;
+  keyDEL   = #83;
+
+Implementation
+
+End.
