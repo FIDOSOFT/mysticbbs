ed84018acefd6a23d3a3707970703eb3886ee069
mysticbbs <mysticbbs@gmail.com>
2012-02-14 05:11:13 -0500

    Initial import of feature/bug tracking source

---
+++ mystic/todo.pas
@@ -0,0 +1,13 @@
+IDEAS
+=====
+- ANSI post-processor for message uploads.
+- ANSI reading support in fullscreen reader
+- Ability to override read-type per message base (usersetting/normal/lightbar)
+- Ability to override index setting per message base (same as above)
+- New ANSI template system
+- Online ANSI file viewer (integrate with art gallery)
+- Online ANSI help system
+
+BUGS
+====
+- QWK does not copy welcome files
