e41b676dc024c23b3d966734614bf6069126e329
mysticbbs <mysticbbs@gmail.com>
2012-09-06 04:34:01 -0400

    TPipe

---
+++ mdl/m_pipe.pas
@@ -0,0 +1,24 @@
+Unit m_Pipe;
+
+{$I M_OPS.PAS}
+
+Interface
+
+{$IFDEF UNIX}
+  Uses m_Pipe_Unix;
+  Type TPipe = Class(TPipeUnix);
+{$ENDIF}
+
+{$IFDEF WINDOWS}
+  Uses m_Pipe_Disk;
+  Type TPipe = Class(TPipeDisk);
+{$ENDIF}
+
+{$IFDEF OS2}
+  Uses m_Pipe_Disk;
+  Type TPipe = Class(TPipeDisk);
+{$ENDIF}
+
+Implementation
+
+End.
