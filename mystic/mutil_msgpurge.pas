3a264377562839e8d22ebf54600a6feaedf756ba
mysticbbs <mysticbbs@gmail.com>
2012-09-08 04:27:43 -0400

    Initial import

---
+++ mystic/mutil_msgpurge.pas
@@ -0,0 +1,25 @@
+Unit MUTIL_MsgPurge;
+
+{$I M_OPS.PAS}
+
+Interface
+
+Procedure uPurgeMessageBases;
+
+Implementation
+
+Uses
+  m_Strings,
+  mUtil_Common,
+  mUtil_Status;
+
+Procedure uPurgeMessageBases;
+Begin
+  ProcessName   ('Purging Message Bases', True);
+  ProcessResult (rWORKING, False);
+
+  ProcessStatus ('Complete');
+  ProcessResult (rDONE, True);
+End;
+
+End.
