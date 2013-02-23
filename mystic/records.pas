e0635cfde71e72b7f430cecf9b0b0c10c9c49e20
mysticbbs <mysticbbs@gmail.com>
2013-02-22 20:44:41 -0500

    A25

--- mystic/records.pas
+++ mystic/records.pas
@@ -23,7 +23,7 @@
 Const
   mysSoftwareID  = 'Mystic';                                    // no idea
   mysCopyYear    = '1997-2012';                                 // its been a long time!
-  mysVersion     = '1.10 A24';                                  // current version
+  mysVersion     = '1.10 A25';                                  // current version
   mysDataChanged = '1.10 A11';                                  // version of last records change
 
   {$IFDEF WIN32}
